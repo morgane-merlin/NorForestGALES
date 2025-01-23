02777dd50a9a52121c0c5b03817c10ff3445dcda
morgane-merlin <morgane.merlin@nibio.no>
2025-01-23 11:34:37 +0100

    update scripts

--- 3.CWS_calculation/FGrou_CWS_SR16.R
+++ 3.CWS_calculation/FGrou_CWS_SR16.R
@@ -1,76 +1,81 @@
 #!/bin/Rscript
 rm(list=ls())
+# We collect the arguments supplied to the Rscript call
 args <- commandArgs(TRUE)
 
 require(pacman)
 ## @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-## packages and functions
-pacman::p_load(purrr, data.table, tidyverse, terra, fgRcpp)
+# The Cpp code for ForestGales was compiled in its own small package for simplifying its use in R - fgRcppNorway
+pacman::p_load(purrr, data.table, tidyverse, terra, fgRcppNorway)
 ## @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 ## @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-## Path to FutureForests forskergruppe
-#working.dir <- commandArgs()[1]
-#setwd(file.path("~", working.dir))
+# We set up the path for saving the files
 path_0 <- args[1]
 
-## @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-
-## @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# Compile the Cpp code for ForestGales
-#sourceCpp(paste0(path_0, "/FG_Rcpp/FGdataprep_Norge_cpp.cpp"))
-#sourceCpp(paste0(path_0, "/FG_Rcpp/FGfunctions_Norge_cpp.cpp"))
 exit <- function() {invokeRestart("abort")}
 ## @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-# get variables from bash
+# We get the variables from the bash arguments
+print("Loading rasters")
+# The tile identifier
 tile_id=args[2]
+# the full tile shapefile - it includes the region (fylke) as it is key for using the proper stem volume equations
 sr16_tiles=vect(args[3])
+# the species raster
 species=rast(args[4])
-mean_ht=rast(args[5])["SRRMHOYDE_1"]
-mean_dbh=rast(args[6])["SRRDIAMMIDDEL_GE8_1"]
-top_height=rast(args[7])["SRROHOYDE_1"]
-NT=rast(args[8])["SRRTREANTALL_GE8_1"]
-BA=rast(args[9])["SRRGRFLATE_1"]
+# the mean tree height raster
+mean_ht=rast(args[5])
+# the mean DBH raster
+mean_dbh=rast(args[6])
+# the top height raster
+top_height=rast(args[7])
+# the number of trees raster
+NT=rast(args[8])
+# the basal area raster
+BA=rast(args[9])
+# note that NT and BA rasters are required to approximate the spacing between trees
+# the ForestGALES soil group classification raster
 soil_group=rast(args[10])
+# the ForestGALES rooting depth classification raster
 rooting=rast(args[11])
-dist_edge_E=args[12]
-gap_size_E=args[13]
-dist_edge_W=args[14]
-gap_size_W=args[15]
-dist_edge_S=args[16]
-gap_size_S=args[17]
-dist_edge_N=args[18]
-gap_size_N=args[19]
+# the distance to the forest edge and size of the upwind gap for the four cardinal directions - those are already at the tile level since the calculations to obtain these rasters were done at the same tile level
+dist_edge_E=rast(args[12])
+gap_size_E=rast(args[13])
+dist_edge_W=rast(args[14])
+gap_size_W=rast(args[15])
+dist_edge_S=rast(args[16])
+gap_size_S=rast(args[17])
+dist_edge_N=rast(args[18])
+gap_size_N=rast(args[19])
+# the seasonal scenario considered
 season=args[20]
+# the directory to save the files
 save_dir=args[21]
+# a text prefix to save the files
 save_prefix=args[22]
+# in case of the winter scenario, we add the snow raster
 if(length(args)== 23){
 snow=rast(args[23])
 }
 
-# group gap rasters
-gap_rasters_E <- c(dist_edge_E, gap_size_E)
-gap_rasters_W <- c(dist_edge_W, gap_size_W)
-gap_rasters_N <- c(dist_edge_N, gap_size_N)
-gap_rasters_S <- c(dist_edge_S, gap_size_S)
-
-
 # 1. select ForestGales parameters ####
 # Load the ForestGales and the species parameters
-fgr_constants_nibio <- read.table(paste0(path_0, "/FGparams/fgr_constants_nibio.txt"), header = T, sep = "\t")
-nibio_params <- read.table(paste0(path_0, "/FGparams/nibio_species_parameters_180624.txt"), sep = "\t", header = T) %>%
-column_to_rownames(var="rownames")
+fgr_constants <- read.table("fgr_constants_1.txt", header = T, sep = "\t")
+sp_params <- read.table("species_parameters_1.txt", sep = "\t", header = T) %>%
+column_to_rownames(var="rownames") %>%
+filter(reference == "Norge") # we filter for the Norwegian parameters
+# if the seasonal scenario is winter, we filter the species parameters to the leafless for birch ("ll") 
 if(grepl("winter", season)){
-nibio_params.sr16 <- nibio_params %>% filter(reference == "Norge" & (leafstatus == "ll" | is.na(leafstatus))) 
+sp_params <- sp_params %>% filter((leafstatus == "ll" | is.na(leafstatus))) 
 } else {
-nibio_params.sr16 <- nibio_params %>% filter(reference == "Norge" & (leafstatus == "lo" | is.na(leafstatus))) 
+sp_params <- sp_params %>% filter((leafstatus == "lo" | is.na(leafstatus))) 
 }  
-
+# we set up the factor coding for Breakage and Overturning
 damage.factors <- tibble(mode_of_damage = c("Breakage", "Overturning")) %>%
 mutate(damage.factor = as.numeric(as.factor(mode_of_damage)))
 
-## extract the tile and fylke of interest
+## We extract the tile and fylke (region) of interest
 fylketile <- subset(sr16_tiles, subset = sr16_tiles$SR16_RUTEN == tile_id)
 ## Check if the tile contains data - some (at least one) doesnt
 if(is.empty(ext(fylketile))){
@@ -78,7 +83,9 @@ message("This tile has no SR16 data.")
 exit()}
 
 
-## Crop the rasters to the tile
+
+
+## Crop the available rasters to the tile
 sp <- crop(species, ext(fylketile))
 if(global(sp, fun="notNA") == 0){
     message("There is no forest data in this tile")
@@ -96,13 +103,91 @@ tht <- crop(top_height, ext(fylketile))
 nt <- crop(NT, ext(fylketile))
 ba <- crop(BA, ext(fylketile))
 
-
-# get buffer around fylketile to include all SR16 pixels (some may be at the very border and thus not get assigned a fylke otherwise)
+# Note that some forest pixels are at the border of the raster and may thus not have been assigned a fylke so we use a buffer
 fylketile <- buffer(fylketile, 100)
-# rasterize to get fylke of each SR16 pixel
+# We rasterize to get fylke of each SR16 pixel
 fylke <- rasterize(fylketile, sp, field = "FYLKE")
 
+# We remove the full rasters
 rm(sr16_tiles, species, soil_group, rooting, mean_ht, mean_dbh, top_height, NT, BA, fylketile)
+# We create a stacked forest raster with all the tile rasters currently available
+forest.0 <- c(sp, mht, mdbh, tht, nt, ba, s_g, root, fylke)
+
+# We loop through the four cardinal directions and calculate the CWS
+for(direction in c("E", "N", "S", "W")){
+  d_e <- get(paste0("dist_edge_", direction))
+  g_s <- get(paste0("gap_size_", direction))
+  # Note that the distance to edge and gap size tiles may be smaller than the other raster tiles because we created them with some constraints which are applicable to the calculation of the CWS
+  # a few lines should be added to check this and crop as required.
+  message(paste0("Direction - ", direction))
+  # prepare output file name
+  output_name <- paste0(save_dir, "/", save_prefix, tile_id, "_", direction)
+  
+  # add the distance to the edge and gap size to the stacked forest raster
+  edges <- crop(c(d_e, g_s), ext(forest.0))
+    forest.sel <- c(forest.0, edges)
+  rm(d_e, g_s) # we clean up and remove the rasters
+  # we name the raster layers properly
+  names(forest.sel) <- c("species", "mean_ht", "mean_dbh", "top_ht", "N8", "BA5", "soil_group", "rooting", "fylke", "dist_edge", "gap_size")
+  if(exists("snow.r")){
+    forest.sel <- c(forest.sel, snow.r)
+    names(forest.sel) <- c("species", "mean_ht", "mean_dbh", "top_ht", "N8", "BA5", "soil_group", "rooting", "fylke", "dist_edge", "gap_size", "snow_load")
+  }
+  # we convert the raster into a dataframe with cell identification numbers and coordinates
+  forest.sel <- as.data.frame(forest.sel, xy = T, cells = T)
+  setDT(forest.sel)
+  setkey(forest.sel, cell, x, y)
+  message(" -- Forest data preparation --")
+  # we remove the the forest pixels not meeting the contraints - pixels with mean height under 10 m (note the units), if mean DBH or BA are null and if number of trees is under 400
+  forest.sel <- forest.sel[mean_ht > 100 & !is.na(mean_dbh) & species != 0 & N8 >= 400,][!(N8 == 0 & BA5 == 0),]
+  # This filter could remove all forest pixels so we catch this possibility
+  if(nrow(forest.sel)== 0){
+    message("No data in this tile fits the requirements")
+    exit()}
+  rm(forest.sel)
+  
+  # Some forest pixels may have odd values (-999) in the soil_group and rooting depth, so we use a simple gap filling using neighboring values 
+  forest.sel[, `:=` (soil_group = nafill(soil_group, type = "locf"), rooting = nafill(rooting, type = "locf")), by = y]
+  # We recode the species factor 
+  forest.sel[, `:=` (species = fcase(species == 1, "NS", species == 2, "SP", species == 3, "BI"),
+                     stand_id = cell)]
+  # We split the dataframe into a dataframe with cell and coordinates information (forest.sel.loc) and a dataframe with the key variables 
+  forest.sel.loc <- copy(forest.sel)[, c("cell", "x", "y")]
+  forest.sel.f <- copy(forest.sel)[, c("stand_id", "species", "mean_dbh", "mean_ht", "spacing", "fylke", "soil_group", "rooting", "dist_edge", "gap_size")]
+  if(exists("snow.r")){
+    forest.sel.f <- copy(forest.sel2)[, c("stand_id", "species", "mean_dbh", "mean_ht", "spacing", "fylke", "soil_group", "rooting", "dist_edge", "gap_size", "snow_load")]
+  }
+  # We run the first RCpp function to prepare the input forest datatable into the full datatable with all required variables for calculating the CWS
+  forest.dataprep <- fg_rou_dataprep_Norge_cpp(forest.sel.f, fgr_constants = fgr_constants, species_parameters = species_params,
+                                               season = season)
+  rm(forest.sel) # we remove the input forest datatable
+  gc() # we clear the R session memory
+  # Calculate the critical wind speed 
+  message(" -- Critical Wind Speed Calculation --")
+  # we use the second RCpp function
+  forest.CWS <- fg_rou_nibio_cpp(forest.dataprep, fgr_constants = fgr_constants_FNFI, species_parameters = species_params, breakage_basecanopy = "no")$results
+  setDT(forest.CWS)
+  # Convert the character variables to numbers
+  forest.CWS <- merge(forest.CWS, damage.factors, by = "mode_of_damage", all.x = T, all.y = F)
+  rm(forest.dataprep)
+  gc()
+  # > Saving files ####
+  message(" -- Saving files --")
+  # Merge the CWS with the coordinates of the cell
+  forest.CWS.f <- merge(forest.CWS[, stand_id := as.numeric(stand_id)], forest.sel.loc, by.x = "stand_id", by.y = "cell", all = T)
+  # Get full coverage for the coordinates -- only perhaps for small dataframes - technically the problem arises only for tiles 558_396 & 582_396 with 12 and 3 pixels respectively
+  # also applies for two more tiles where only one pixel was found so need to expand a bit to be able to create a raster - tiles 446_228 & 590_380 & 518_316 & 574_404
+  if(length(forest.CWS.f$x)<=15){
+    xyfull <- setDT(expand.grid(x = seq(min(forest.sel.loc$x), max(forest.sel.loc$x)+16, 16),
+                                y = seq(min(forest.sel.loc$y), max(forest.sel.loc$y)+16, 16)))
+    forest.CWS.f <- merge(forest.CWS.f, xyfull, by = c("x", "y"), all = T)
+  }
+  forest.CWS.f <- terra::rast(forest.CWS.f[, c("x", "y", "u_elev_damage", "damage.factor", "u_elev_b", "u_elev_o")], type = "xyz", crs = "epsg:32632")
+  terra::writeRaster(forest.CWS.f, filename = paste0(output_name, ".tif"), filetype = "GTiff", overwrite = TRUE, gdal = c("COMPRESS=DEFLATE", "TFW=YES"))
+  rm(forest.CWS, forest.CWS.f)
+  gc()
+}
+
 
 #Loop through the four directions
 for(gap_rasters in list(gap_rasters_E, gap_rasters_W, gap_rasters_N, gap_rasters_S)){
@@ -146,11 +231,11 @@ forest.sel.f <- copy(forest.sel)[, c("stand_id", "fylke", "species", "mean_dbh",
 if(exists("snow.r")){
 forest.sel.f <- copy(forest.sel)[, c("stand_id", "fylke", "species", "mean_dbh", "top_ht", "mean_ht", "spacing", "soil_group", "rooting", "dist_edge", "gap_size", "snow_load")]
 }
-forest.dataprep <- fg_rou_dataprep_Norge_cpp(forest.sel.f, fgr_constants = fgr_constants_nibio, species_parameters = nibio_params.sr16, season = season)
+forest.dataprep <- fg_rou_dataprep_Norge_cpp(forest.sel.f, fgr_constants = fgr_constants_nibio, species_parameters = sp_params.sr16, season = season)
 
 # Calculate the critical wind speed 
 message(" -- Critical Wind Speed Calculation --")
-forest.CWS <- fg_rou_nibio_cpp(forest.dataprep, fgr_constants = fgr_constants_nibio, species_parameters = nibio_params.sr16, breakage_basecanopy = "no")$results
+forest.CWS <- fg_rou_nibio_cpp(forest.dataprep, fgr_constants = fgr_constants_nibio, species_parameters = sp_params.sr16, breakage_basecanopy = "no")$results
 setDT(forest.CWS)
 # Convert the character variables to numbers
 forest.CWS <- merge(forest.CWS, damage.factors, by = "mode_of_damage", all.x = T, all.y = F)
